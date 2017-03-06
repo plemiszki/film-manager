50768e1299f3efb9249084f74df99571480bfb8e
Peter Lemiszki <plemiszki@gmail.com>
2017-03-05 21:51:46 -0500

    update sign in

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,5 @@
 Clearance.configure do |config|
   config.allow_sign_up = false
   config.mailer_sender = "reply@example.com"
-  config.redirect_url = "/admin"
   config.routes = false # set to false because we're using custom Clearance routes
 end
