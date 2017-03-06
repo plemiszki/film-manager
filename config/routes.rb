711a8072e721b8567e3c5d2c810738a4c524030b
Peter Lemiszki <plemiszki@gmail.com>
2017-03-05 20:59:12 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
 
   # Clearance ------------------------
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resource :session, controller: "clearance/sessions", only: [:create]
-  resources :users, controller: "clearance/users", only: [:create] do
-    resource :password,
-      controller: "clearance/passwords",
-      only: [:create, :edit, :update]
-  end
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   # ----------------------------------
