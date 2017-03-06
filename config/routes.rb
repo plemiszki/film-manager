9a06dbd177d6c1516359b957dd724135f29d8315
Peter Lemiszki <plemiszki@gmail.com>
2017-03-05 20:55:04 -0500

    add clearance routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   root to: "films#index"
   resources :films, only: [:index, :show]
   resources :users, only: [:index, :show]
+
+  # Clearance ------------------------
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  # ----------------------------------
 end
