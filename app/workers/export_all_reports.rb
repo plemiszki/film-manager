938e02746cf9ad93dcb764988837f3a94c022ecb
Peter Lemiszki <plemiszki@gmail.com>
2017-07-18 00:20:23 -0400

    change p statement

--- app/workers/export_all_reports.rb
+++ app/workers/export_all_reports.rb
@@ -21,12 +21,14 @@ class ExportAllReports
     reports.each do |report|
       return if Sidekiq.redis {|c| c.exists("cancelled-#{jid}") }
       if days_due == "all" || report.film.days_statement_due == days_due.to_i
-        p '---------------------------'
-        p "#{report.film.title} (#{jid})"
-        p '---------------------------'
+        # p '---------------------------'
+        # p "#{report.film.title} (#{jid})"
+        # p '---------------------------'
         report.calculate!
         save_path = report.amount_due > 0 ? "#{job_folder}/amount due" : "#{job_folder}/no amount due"
-        p save_path
+        p '---------------------------'
+        p save_path if report.film.title == "And They Call It Summer"
+        p '---------------------------'
         report.export!(save_path)
         job.update({current_value: job.current_value + 1})
       end
