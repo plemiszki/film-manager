9ab68d128d86db9570a0134cd7b02ea4751608ec
Peter Lemiszki <plemiszki@gmail.com>
2017-10-04 14:21:39 -0400

    add search bar to modal select

--- flux/components/modal-select.jsx
+++ flux/components/modal-select.jsx
@@ -4,19 +4,23 @@ var ModalSelect = React.createClass({
 
   getInitialState: function() {
     return({
-      options: this.props.options
+      options: this.props.options,
+      searchText: ""
     });
   },
 
   render: function() {
     return(
-      <ul className="licensor-modal-list">
-        {this.state.options.map(function(option, index) {
-          return(
-            <li key={index} onClick={this.props.func} data-id={option.id}>{option[this.props.property]}</li>
-          );
-        }.bind(this))}
-      </ul>
+      <div className="modal-select">
+        <input className="search-box" onChange={Common.changeSearchText.bind(this)} value={this.state.searchText} data-field="searchText" />
+        <ul className="licensor-modal-list">
+          {this.state.options.filterSearchText(this.state.searchText).map(function(option, index) {
+            return(
+              <li key={index} onClick={this.props.func} data-id={option.id}>{option[this.props.property]}</li>
+            );
+          }.bind(this))}
+        </ul>
+      </div>
     )
   }
 
