bb528dbc82981e6c269335bbfd3d5694f8ef6f6a
Peter Lemiszki <plemiszki@gmail.com>
2018-03-19 10:53:22 -0400

    order films endpoint by id

--- app/controllers/api/website_controller.rb
+++ app/controllers/api/website_controller.rb
@@ -1,7 +1,7 @@
 class Api::WebsiteController < CyberController
 
   def films
-    @films = Film.all.includes(:label, :directors, :actors, :laurels, :quotes, :related_films, dvds: [:dvd_type, :dvd_shorts])
+    @films = Film.all.includes(:label, :directors, :actors, :laurels, :quotes, :related_films, dvds: [:dvd_type, :dvd_shorts]).order(:id)
     render "films.json.jbuilder"
   end
 
