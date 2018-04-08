d67402fb7735dda32ec0c24439dca4d3ccf2a0f7
Peter Lemiszki <plemiszki@gmail.com>
2018-04-08 12:58:26 -0400

    overage booking export fix

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -188,9 +188,9 @@ class Invoice < ActiveRecord::Base
         string += '</table><div class="page-break"><table><tr><th>Item</th><th>Unit Price</th><th>Qty</th><th>Total Price</th></tr>'
       end
       string += "<tr>"
-      if row.item_label == "Advance" || row.item_label[0..3] == "Owed"
-        gross = row.item_label[5..-1] if row.item_label[0..3] == "Owed"
-        string += "<td class='big-margin'>#{booking.film.title}<br>#{booking.start_date.strftime("%-m/%-d/%y")} - #{booking.end_date.strftime("%-m/%-d/%y")}<br>#{booking.screenings} screening#{booking.screenings > 1 ? 's' : ''}<br>#{booking.terms + (row.item_label[0..3] == "Owed" ? " #{row.item_label[5..-1]}" : "")}<br>#{booking.format.name}</td>"
+      if row.item_label == "Advance" || row.item_label[0..6] == "Overage"
+        gross = row.item_label[5..-1] if row.item_label[0..6] == "Overage"
+        string += "<td class='big-margin'>#{booking.film.title}<br>#{booking.start_date.strftime("%-m/%-d/%y")} - #{booking.end_date.strftime("%-m/%-d/%y")}<br>#{booking.screenings} screening#{booking.screenings > 1 ? 's' : ''}<br>#{booking.terms + (row.item_label[0..6] == "Overage" ? " #{row.item_label[7..-1]}" : "")}<br>#{booking.format.name}</td>"
       elsif row.item_label == "Shipping Fee"
         string += "<td class='big-margin'>#{row.item_label}</td>"
       else
@@ -201,7 +201,7 @@ class Invoice < ActiveRecord::Base
       else
         string += "<td></td><td></td>"
       end
-      if row.item_label == "Advance" || row.item_label[0..3] == "Owed"
+      if row.item_label == "Advance" || row.item_label[0..6] == "Overage"
         string += "<td class='big-margin'><br><br><br><br>#{dollarify(row.total_price.to_s)}</td>"
       elsif row.item_label == "Shipping Fee"
         string += "<td class='big-margin'>#{dollarify(row.total_price.to_s)}</td>"
