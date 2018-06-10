582eefeb713f70c7d2f459be845ac0dd96a3d6d7
Peter Lemiszki <plemiszki@gmail.com>
2018-06-10 15:53:43 -0400

    creating new territory adds it to films with worldwide rights

--- app/controllers/api/territories_controller.rb
+++ app/controllers/api/territories_controller.rb
@@ -13,6 +13,10 @@ class Api::TerritoriesController < AdminController
   def create
     @territory = Territory.new(territory_params)
     if @territory.save
+      film_rights_with_worldwide_rights = FilmRight.where(territory_id: 1)
+      film_rights_with_worldwide_rights.each do |film_right|
+        FilmRight.create!(film_id: film_right.film_id, territory_id: @territory.id, right_id: film_right.right_id, start_date: film_right.start_date, end_date: film_right.end_date, exclusive: film_right.exclusive, value: true)
+      end
       @territories = Territory.all
       render 'index.json.jbuilder'
     else
