c00c88a8c86c345f2f27a1c6baf04331b819c4f3
Peter Lemiszki <plemiszki@gmail.com>
2018-06-25 15:25:58 -0400

    fix update stock bug

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   get '/import' => 'films#import_data'
   patch '/users/1' => 'films#upload'
   patch '/users/2' => 'api/royalty_reports#import'
-  patch '/users/3' => 'api/dvds#update_stock'
+  patch '/users/4' => 'api/dvds#update_stock'
   resources :countries, only: [:show]
   resources :languages, only: [:show]
   resources :genres, only: [:show]
