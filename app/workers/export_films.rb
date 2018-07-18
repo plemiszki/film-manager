25f5dce2072ac9d83e5b9e2481d228f904f4f583
Peter Lemiszki <plemiszki@gmail.com>
2018-07-18 08:12:46 -0400

    add rights columns to metadata export

--- app/workers/export_films.rb
+++ app/workers/export_films.rb
@@ -28,10 +28,25 @@ class ExportFilms
       'Countries',
       'Languages',
       'Genres',
-      'Topics'
+      'Topics',
+      'Theatrical',
+      'Educational',
+      'Festival',
+      'Other NT',
+      'SVOD',
+      'TVOD (Cable)',
+      'EST/DTR',
+      'Pay TV',
+      'Free TV',
+      'FVOD',
+      'AVOD',
+      'DVD/Video',
+      'Hotels',
+      'Airlines',
+      'Ships'
     ])
 
-    films = Film.where(id: film_ids).order(:title).includes(:licensor, :label, :laurels)
+    films = Film.where(id: film_ids).order(:title).includes(:licensor, :label, :laurels, :film_rights)
     films.each_with_index do |film, film_index|
       sheet.add_row([
         film.title,
@@ -51,7 +66,22 @@ class ExportFilms
         film.countries.map(&:name).join(", "),
         film.languages.map(&:name).join(", "),
         film.genres.map(&:name).join(", "),
-        film.topics.map(&:name).join(", ")
+        film.topics.map(&:name).join(", "),
+        film.film_rights.select { |right| right.right_id == 1 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 2 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 3 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 4 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 5 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 6 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 7 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 8 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 9 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 10 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 11 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 12 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 13 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 14 }.length > 0 ? 'Yes' : 'No',
+        film.film_rights.select { |right| right.right_id == 15 }.length > 0 ? 'Yes' : 'No',
       ])
       job.update({ current_value: film_index + 1 })
     end
