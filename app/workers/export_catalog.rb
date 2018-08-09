b7c6572c830ecbf974a96db7a326292f57e21cc4
Peter Lemiszki <plemiszki@gmail.com>
2018-08-09 08:40:39 -0400

    add black image

--- app/workers/export_catalog.rb
+++ app/workers/export_catalog.rb
@@ -10,14 +10,31 @@ class ExportCatalog
     require 'caracal'
     file_path = "/tmp/#{time_started}/catalog.docx"
     document = Caracal::Document.save(file_path) do |docx|
-      docx.p 'Lorem ipsum dolor....'
+      films = Film.where(id: film_ids).order(:title).includes(:licensor, :label, :laurels, :film_rights)
+      films.each_with_index do |film, film_index|
+
+        c1 = Caracal::Core::Models::TableCellModel.new margins: { top: 0, bottom: 100, left: 0, right: 0 } do
+          if film.artwork_url
+            img film.artwork_url, width: 200, height: 280
+          else
+            img 'https://film-movement.herokuapp.com/black.jpg', width: 200, height: 280
+          end
+        end
+        c2 = Caracal::Core::Models::TableCellModel.new margins: { top: 0, bottom: 100, left: 0, right: 0 } do
+          p film.title
+          p
+          p film.synopsis
+        end
+
+        docx.table [[c1,c2]] do
+          cell_style cols[0], width: 4500
+        end
+
+        job.update({ current_value: film_index + 1 })
+      end
+      job.update({ first_line: 'Saving Word Document', second_line: false })
     end
 
-    films = Film.where(id: film_ids).order(:title).includes(:licensor, :label, :laurels, :film_rights)
-    films.each_with_index do |film, film_index|
-      job.update({ current_value: film_index + 1 })
-    end
-    
     job.update({ first_line: 'Uploading to AWS' })
     s3 = Aws::S3::Resource.new(
       credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
