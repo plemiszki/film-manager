29ccd2fbd4e608aea59271c33439a3a027d57fd9
Peter Lemiszki <plemiszki@gmail.com>
2018-12-15 16:53:19 -0500

    create/destroy reverse crossed film

--- app/controllers/api/crossed_films_controller.rb
+++ app/controllers/api/crossed_films_controller.rb
@@ -3,6 +3,7 @@ class Api::CrossedFilmsController < AdminController
   def create
     @crossed_film = CrossedFilm.new(crossed_film_params)
     if @crossed_film.save
+      CrossedFilm.create!(film_id: @crossed_film.crossed_film_id, crossed_film_id: @crossed_film.film_id)
       @crossed_films = CrossedFilm.where(film_id: @crossed_film.film_id)
       @other_crossed_films = Film.where.not(id: ([@crossed_film.film_id] + @crossed_films.pluck(:crossed_film_id)), film_type: 'Short')
       render 'index.json.jbuilder'
@@ -13,7 +14,8 @@ class Api::CrossedFilmsController < AdminController
 
   def destroy
     @crossed_film = CrossedFilm.find(params[:id])
-    @crossed_film.destroy
+    @crossed_film.destroy!
+    CrossedFilm.find_by(film_id: @crossed_film.crossed_film_id, crossed_film_id: @crossed_film.film_id).destroy!
     @crossed_films = CrossedFilm.where(film_id: @crossed_film.film_id)
     @other_crossed_films = Film.where.not(id: ([@crossed_film.film_id] + @crossed_films.pluck(:crossed_film_id)), film_type: 'Short')
     render 'index.json.jbuilder'
