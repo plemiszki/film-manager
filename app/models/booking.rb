c291576a06afdb0860dbfae92003f9daa5d539ff
Peter Lemiszki <plemiszki@gmail.com>
2019-07-25 21:06:02 -0400

    automated bo reminders

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -24,4 +24,64 @@ class Booking < ActiveRecord::Base
     end
   end
 
+  def needs_reminder
+    case booking_type
+    when "Theatrical"
+      return true
+    when "Festival"
+      return terms.match(/^\$[\d\.]+ vs [\d\.]+%$/) || terms.match(/^[\d\.]+%$/)
+    else
+      return false
+    end
+  end
+
+  def self.send_box_office_reminders(name:, email:)
+
+email_body = <<-COPY
+Hello,
+
+This is an automated request to remind you that we have not received the box office report for this booking. Please send the report to my attention by end of day.
+
+Kind Regards,
+
+#{name}
+Film Movement
+
+COPY
+
+    theatrical_bookings = Booking.where(booking_type: "Theatrical", status: "Confirmed", box_office_received: false, box_office: 0, terms_change: false)
+    .where("end_date < ?", Date.today - 4.weeks)
+    .where("end_date > ?", Date.today - 6.months)
+    .order(:start_date)
+
+    bookings_with_missing_weekly_box_office = []
+    Booking.where(booking_type: "Theatrical", status: "Confirmed", box_office_received: false, terms_change: true).includes(:weekly_box_offices).each do |booking|
+      bookings_with_missing_weekly_box_office << booking if booking.weekly_box_offices.length == 0
+    end
+
+    theatrical_bookings += bookings_with_missing_weekly_box_office
+
+    festival_bookings = Booking.where(booking_type: "Festival", status: "Confirmed", box_office_received: false, box_office: 0)
+    .where("end_date < ?", Date.today - 4.weeks)
+    .where("end_date > ?", Date.today - 6.months)
+    .order(:start_date).select do |booking|
+      booking.needs_reminder
+    end
+
+    all_bookings = theatrical_bookings + festival_bookings
+    mg_client = Mailgun::Client.new ENV['MAILGUN_KEY']
+    all_bookings.each_with_index do |booking, index|
+      venue = booking.venue
+      venue_name = (venue.billing_name.present? ? venue.billing_name : venue.label)
+      message_params = {
+        from: email,
+        to: 'plemiszki@gmail.com',
+        # bcc: 'plemiszki@gmail.com',
+        subject: "Box Office Reminder: #{booking.film.title} at #{venue_name} (#{booking.start_date.strftime("%-m/%-d/%y")})",
+        text: email_body
+      }
+      mg_client.send_message 'filmmovement.com', message_params
+    end
+  end
+
 end
