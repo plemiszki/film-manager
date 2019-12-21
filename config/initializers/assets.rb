2db62836fea251f2a1e07b9e3eea5c5103686f69
Peter Lemiszki <plemiszki@gmail.com>
2019-12-21 14:17:52 -0500

    remove search.js from assets config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules/handy-com
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
