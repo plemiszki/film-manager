268c19b137b3f2b7aa62f790ec9f86a68afefd05
Peter Lemiszki <plemiszki@gmail.com>
2020-07-31 09:46:13 -0400

    delete converted bookings method

--- app/models/virtual_booking.rb
+++ app/models/virtual_booking.rb
@@ -6,8 +6,7 @@ class VirtualBooking < ActiveRecord::Base
   belongs_to :film
   belongs_to :venue
 
-  def self.convert_virtual!
-    missing_venues = []
+  def self.delete_converted!
     file_path = Rails.root.join("./virtual-bookings.xlsx").to_s
     xlsx = Roo::Spreadsheet.open(file_path)
     sheet = xlsx.sheet(0)
@@ -19,29 +18,8 @@ class VirtualBooking < ActiveRecord::Base
         index += 1
         next
       end
-      venue_name = columns[3].strip
-      venue_name = "Time and Space Limited" if venue_name == "Time & Space Ltd."
-      venue_name = "Time and Space Limited" if venue_name == "Time & Space Limited"
-      venue_name = "Corazon Cinema and Cafe" if venue_name == "Corazon Cinema and Café"
-      venue_name = "Gateway Film Center 8" if venue_name == "Gateway Film Center"
-      venue_name = "SIE Film Center" if venue_name == "SIE FilmCenter"
-      venue = Venue.find_by_label(venue_name)
       booking_id = columns[35]
-      booking = Booking.find(booking_id)
-      city = columns[23]
-      state = columns[24]
-      url = columns[34] || ""
-      VirtualBooking.create!(
-        film_id: booking.film.id,
-        venue_id: venue.id,
-        date_added: booking.date_added,
-        start_date: booking.start_date,
-        end_date: booking.end_date,
-        shipping_city: city,
-        shipping_state: state,
-        terms: booking.terms,
-        url: url
-      )
+      Booking.find(booking_id).destroy
       index += 1
     end
   end
