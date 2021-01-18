7e5072f92b2096c4bceed7158eb563d2ead56981
Peter Lemiszki <plemiszki@gmail.com>
2021-01-18 12:11:39 -0500

    install textacular gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_192000) do
+ActiveRecord::Schema.define(version: 2021_01_18_170910) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "actors", id: :serial, force: :cascade do |t|
