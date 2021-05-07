e11ee3dbc59f68dc42f894b33f08661312ebd3c5
Peter Lemiszki <plemiszki@gmail.com>
2021-05-07 14:19:06 -0400

    add order to countries, genres, topics associations

--- app/models/film.rb
+++ app/models/film.rb
@@ -41,10 +41,10 @@ class Film < ActiveRecord::Base
   has_many :related_films, dependent: :destroy
   has_many :directors, -> { order(:order) }, dependent: :destroy
   has_many :actors, as: :actorable, dependent: :destroy
-  has_many :film_countries, dependent: :destroy
+  has_many :film_countries, -> { order(:order) }, dependent: :destroy
   has_many :film_languages, -> { order(:order) }, dependent: :destroy
-  has_many :film_genres, dependent: :destroy
-  has_many :film_topics, dependent: :destroy
+  has_many :film_genres, -> { order(:order) }, dependent: :destroy
+  has_many :film_topics, -> { order(:order) }, dependent: :destroy
   has_many :countries, through: :film_countries
   has_many :languages, through: :film_languages
   has_many :film_formats, dependent: :destroy
