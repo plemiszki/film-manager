5923f596c36934e312bc484fc44721ded8d8dd10
Peter Lemiszki <plemiszki@gmail.com>
2021-07-06 09:53:37 -0400

    update test

--- spec/features/genre_details_spec.rb
+++ spec/features/genre_details_spec.rb
@@ -15,6 +15,7 @@ describe 'genre_details', type: :feature do
 
   it 'displays information about the genre' do
     visit genre_path(@genre, as: $admin_user)
+    wait_for_ajax
     expect(find('input[data-field="name"]').value).to eq('Comedy')
   end
 
