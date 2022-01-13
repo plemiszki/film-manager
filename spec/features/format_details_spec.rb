6fe0220d832520acdbce5da67dd7a0df43b1e3d8
Peter Lemiszki <plemiszki@gmail.com>
2022-01-13 18:59:55 -0500

    add spinner wait to format details spec

--- spec/features/format_details_spec.rb
+++ spec/features/format_details_spec.rb
@@ -15,6 +15,7 @@ describe 'format_details', type: :feature do
 
   it 'displays information about the format' do
     visit format_path(@format, as: $admin_user)
+    wait_for_ajax
     expect(find('input[data-field="name"]').value).to eq('35mm')
   end
 
