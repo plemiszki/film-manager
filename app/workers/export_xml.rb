d3c7ae7553a23a792acb26187a7222ec8182f7ad
Peter Lemiszki <plemiszki@gmail.com>
2023-08-19 15:21:52 -0400

    export xml file

--- app/workers/export_xml.rb
+++ app/workers/export_xml.rb
@@ -7,12 +7,23 @@ class ExportXml
     job_folder = "#{Rails.root}/tmp/#{time_started}"
     FileUtils.mkdir_p("#{job_folder}")
 
-    file = File.open("#{job_folder}/test.txt", 'w')
-    file.puts "hello"
-    file.close
+    file = File.open("#{job_folder}/test.xml", 'w')
 
-    # require 'builder'
-    # ...
+    require 'builder'
+    builder = Builder::XmlMarkup.new(target: file)
+    builder.instruct!
+
+    builder << "\n\n"
+    builder.tag!("md:TitleSort")
+
+    builder << "\n\n"
+    builder.tag!("md:ArtReference", resolution: "1920x2560", purpose: "boxart") { builder << "Enter_the_3x4_boxart_file_name_here" }
+    builder << "\n"
+    builder.tag!("md:ArtReference", resolution: "3840x2160", purpose: "cover") { builder << "Enter_the_16x9_cover_file_name_here" }
+    builder << "\n"
+    builder.tag!("md:ArtReference", resolution: "3840x2160", purpose: "hero") { builder << "Enter_the_16x9_hero_file_name_here" }
+
+    file.close
 
     job.update({ first_line: "Uploading to AWS" })
     s3 = Aws::S3::Resource.new(
@@ -20,7 +31,7 @@ class ExportXml
       region: 'us-east-1',
     )
     bucket = s3.bucket(ENV['S3_BUCKET'])
-    obj = bucket.object("#{time_started}/test.txt")
+    obj = bucket.object("#{time_started}/test.xml")
     obj.upload_file(file.path, acl:'public-read')
 
     job.update!({ status: 'success', first_line: '', metadata: { url: obj.public_url }, errors_text: '' })
