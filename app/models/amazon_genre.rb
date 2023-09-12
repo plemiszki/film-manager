0982f75cb90339cf324641913496fd3a5292c809
Peter Lemiszki <plemiszki@gmail.com>
2023-09-12 17:32:06 +0200

    dependent destroy

--- app/models/amazon_genre.rb
+++ app/models/amazon_genre.rb
@@ -2,6 +2,8 @@ class AmazonGenre < ActiveRecord::Base
 
   validates :code, presence: true
 
+  has_many :amazon_genre_films, dependent: :destroy
+
   def self.import!
     table = CSV.parse(File.read(Rails.root.join("./amazon_genres.csv").to_s))
     table.each do |row|
--- app/models/amazon_language.rb
+++ app/models/amazon_language.rb
@@ -2,6 +2,8 @@ class AmazonLanguage < ActiveRecord::Base
 
   validates :name, :code, presence: true
 
+  has_many :amazon_language_films, dependent: :destroy
+
   def self.import!
     table = CSV.parse(File.read(Rails.root.join("./amazon_languages.csv").to_s))
     table.each do |row|
