39e13a8b55f1ed67f9fa6ecae15bf8be9510e028
Peter Lemiszki <plemiszki@gmail.com>
2023-12-03 10:00:50 -0500

    date format - credit memos

--- spec/features/credit_memo_details_spec.rb
+++ spec/features/credit_memo_details_spec.rb
@@ -23,7 +23,7 @@ describe 'credit_memo_details', type: :feature do
   it 'displays information about the credit_memo' do
     visit credit_memo_path(@credit_memo, as: $admin_user)
     expect(page).to have_content 'CM1'
-    expect(page).to have_content Date.today.strftime('%-m/%-d/%y')
+    expect(page).to have_content Date.today.strftime('%-m/%-d/%Y')
     expect(page).to have_content '012345678'
     expect(page).to have_content 'DVD Vendor'
     expect(page).to have_content 'Billing Name'
