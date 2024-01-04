ab428f55aa26dd6841aa1c3dcc4e86bdd8fbe5cb
Peter Lemiszki <plemiszki@gmail.com>
2024-01-04 16:44:28 -0600

    institution orders - copy over addresses

--- app/models/institution_order.rb
+++ app/models/institution_order.rb
@@ -2,6 +2,8 @@ class InstitutionOrder < ActiveRecord::Base
 
   include DateFieldYearsConverter
 
+  after_create :add_addresses
+
   belongs_to :institution
   alias_attribute :customer, :institution
 
@@ -11,5 +13,24 @@ class InstitutionOrder < ActiveRecord::Base
   validates :order_date, date: true
   validates :materials_sent, date: true, allow_nil: true
   validates :number, uniqueness: { scope: :institution_id }
+
+  def add_addresses
+    institution = Institution.find(institution_id)
+    self.billing_name = institution.billing_name
+    self.billing_address_1 = institution.billing_address_1
+    self.billing_address_2 = institution.billing_address_2
+    self.billing_city = institution.billing_city
+    self.billing_state = institution.billing_state
+    self.billing_zip = institution.billing_zip
+    self.billing_country = institution.billing_country
+    self.shipping_name = institution.shipping_name
+    self.shipping_address_1 = institution.shipping_address_1
+    self.shipping_address_2 = institution.shipping_address_2
+    self.shipping_city = institution.shipping_city
+    self.shipping_state = institution.shipping_state
+    self.shipping_zip = institution.shipping_zip
+    self.shipping_country = institution.shipping_country
+    self.save!
+  end
 
 end
