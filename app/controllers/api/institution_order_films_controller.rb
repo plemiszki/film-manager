6ca6f3a7b690c36c5ac658f40e5ea617b715403f
Peter Lemiszki <plemiszki@gmail.com>
2024-01-06 07:10:25 -0600

    institution order films - controller skeleton

---
+++ app/controllers/api/institution_order_films_controller.rb
@@ -0,0 +1,7 @@
+class Api::InstitutionOrderFilmsController < AdminController
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,12 @@ module FilmManager
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.generators do |g|
+      g.request_specs = false
+      g.helper = false
+      g.view_specs = false
+      g.skip_routes = true
+    end
+
   end
 end
