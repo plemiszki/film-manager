90392fc8f5fb9d522f972cdd7c47629d5dc381ef
Peter Lemiszki <plemiszki@gmail.com>
2024-04-06 11:26:49 -0400

    install ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,36 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   context: __dirname,
-  entry: './frontend/entry.jsx',
+  entry: "./frontend/entry.jsx",
   output: {
-    path: __dirname + '/app/assets/javascripts/me',
-    filename: 'bundle.js',
-    devtoolModuleFilenameTemplate: '[resourcePath]',
-    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+    path: __dirname + "/app/assets/javascripts/me",
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: "[resourcePath]",
+    devtoolFallbackModuleFilenameTemplate: "[resourcePath]?[hash]",
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
+        use: [{ loader: "babel-loader" }],
         exclude: /node_modules/,
-        use: [
-          { loader: 'babel-loader' }
-        ]
-      }
-    ]
+      },
+      {
+        test: /\.tsx?$/,
+        use: [{ loader: "ts-loader" }],
+        exclude: /node_modules/,
+      },
+    ],
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx", ".tsx"],
     alias: {
-      react: path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
-      'react-redux': path.resolve('./node_modules/react-redux'),
-    }
-  }
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+      "react-redux": path.resolve("./node_modules/react-redux"),
+    },
+  },
 };
