43bd7bc75e957b0d9d951e4b48b974eca915dbbf
Peter Lemiszki <plemiszki@gmail.com>
2024-05-19 13:06:15 -0400

    licensor details - styling

--- frontend/components/licensor-details.jsx
+++ frontend/components/licensor-details.jsx
@@ -87,71 +87,65 @@ export default class LicensorDetails extends React.Component {
   render() {
     const { justSaved, changesToSave, spinner } = this.state;
     return (
-      <>
-        <div className="licensor-details">
-          <div className="handy-component">
-            <h1>Licensor Details</h1>
-            <div className="white-box">
-              <div className="row">
-                {Details.renderField.bind(this)({
-                  columnWidth: 4,
-                  entity: "licensor",
-                  property: "name",
-                })}
-                {Details.renderField.bind(this)({
-                  columnWidth: 5,
-                  entity: "licensor",
-                  property: "email",
-                  columnHeader: "Royalty Emails",
-                })}
-                {Details.renderField.bind(this)({
-                  columnWidth: 3,
-                  entity: "licensor",
-                  property: "sageId",
-                  columnHeader: "Sage ID",
-                })}
-              </div>
-              <div className="row">
-                {Details.renderField.bind(this)({
-                  type: "textbox",
-                  columnWidth: 12,
-                  entity: "licensor",
-                  property: "address",
-                  rows: 5,
-                })}
-              </div>
-              <div className="row">
-                <div className="col-xs-12">
-                  <Table
-                    style={{ marginBottom: 60 }}
-                    columns={["title"]}
-                    rows={this.state.films}
-                    urlPrefix="films"
-                    sortable={false}
-                  />
-                </div>
-              </div>
-              <BottomButtons
-                entityName="licensor"
-                confirmDelete={Details.confirmDelete.bind(this)}
-                justSaved={justSaved}
-                changesToSave={changesToSave}
-                disabled={spinner}
-                clickSave={() => {
-                  this.clickSave();
-                }}
-              />
-              <GrayedOut visible={spinner} />
-              <Spinner visible={spinner} />
+      <div className="licensor-details">
+        <div className="handy-component">
+          <h1>Licensor Details</h1>
+          <div className="white-box">
+            <div className="row">
+              {Details.renderField.bind(this)({
+                columnWidth: 4,
+                entity: "licensor",
+                property: "name",
+              })}
+              {Details.renderField.bind(this)({
+                columnWidth: 5,
+                entity: "licensor",
+                property: "email",
+                columnHeader: "Royalty Emails",
+              })}
+              {Details.renderField.bind(this)({
+                columnWidth: 3,
+                entity: "licensor",
+                property: "sageId",
+                columnHeader: "Sage ID",
+              })}
             </div>
+            <div className="row">
+              {Details.renderField.bind(this)({
+                type: "textbox",
+                columnWidth: 12,
+                entity: "licensor",
+                property: "address",
+                rows: 5,
+              })}
+            </div>
+            <div className="row">
+              <div className="col-xs-12">
+                <Table
+                  style={{ marginBottom: 30 }}
+                  columns={["title"]}
+                  rows={this.state.films}
+                  urlPrefix="films"
+                  sortable={false}
+                />
+              </div>
+            </div>
+            <hr />
+            <BottomButtons
+              entityName="licensor"
+              confirmDelete={Details.confirmDelete.bind(this)}
+              justSaved={justSaved}
+              changesToSave={changesToSave}
+              disabled={spinner}
+              clickSave={() => {
+                this.clickSave();
+              }}
+            />
+            <GrayedOut visible={spinner} />
+            <Spinner visible={spinner} />
           </div>
         </div>
-        <style jsx>{`
-          table {
-            margin-bottom: 60px;
-          }
-        `}</style>
-      </>
+      </div>
     );
   }
 }
