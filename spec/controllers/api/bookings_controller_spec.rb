826fe49b5b23e1fa0e69365962456527caa97bfb
Peter Lemiszki <plemiszki@gmail.com>
2024-09-24 09:41:49 -0400

    bookings controller stripe tests

---
+++ spec/controllers/api/bookings_controller_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+require 'support/controllers_helper'
+
+RSpec.describe Api::BookingsController do
+
+  before(:each) do
+    stub_request(
+      :get,
+      "https://api.stripe.com/v1/customers/search?query=email:'someone@somevenue.com'"
+    ).to_return(
+      body: {
+        data: [],
+      }.to_json
+    )
+
+    stub_request(
+      :get,
+      "https://api.stripe.com/v1/customers/search?query=email:'bob@somevenue.com'"
+    ).to_return(
+      body: {
+        data: [{
+          id: "asdf",
+        }],
+      }.to_json
+    )
+  end
+
+  context '#show' do
+    render_views
+
+    it 'returns a blank stripe ID if no customer with the email address exists in Stripe' do
+      booking = create(:booking)
+      get :show, params: { id: booking.id }
+      expect(response).to render_template('api/bookings/show', formats: [:json], handlers: [:jbuilder])
+      expect(response.status).to eq(200)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response["booking"]["stripeId"]).to eq("")
+    end
+
+    it 'returns the stripe ID if a customer with the email address exists in Stripe' do
+      booking = create(:booking, email: "bob@somevenue.com")
+      get :show, params: { id: booking.id }
+      expect(response).to render_template('api/bookings/show', formats: [:json], handlers: [:jbuilder])
+      expect(response.status).to eq(200)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response["booking"]["stripeId"]).to eq("asdf")
+    end
+
+    it 'returns the stripe ID until the email address is changed' do
+      booking = create(:booking, email: "bob@somevenue.com")
+      get :show, params: { id: booking.id }
+      expect(response).to render_template('api/bookings/show', formats: [:json], handlers: [:jbuilder])
+      expect(response.status).to eq(200)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response["booking"]["stripeId"]).to eq("asdf")
+
+      booking.update!(email: "someone@somevenue.com")
+
+      get :show, params: { id: booking.id }
+      expect(response).to render_template('api/bookings/show', formats: [:json], handlers: [:jbuilder])
+      expect(response.status).to eq(200)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response["booking"]["stripeId"]).to eq("")
+    end
+
+  end
+
+end
