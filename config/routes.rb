f9f244253fa4877c6f738c63f07cde00b1445b54
Peter Lemiszki <plemiszki@gmail.com>
2024-10-06 15:09:20 -0400

    sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  # require 'sidekiq/web'
-  # mount Sidekiq::Web => '/sidekiq'
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
 
   root to: "films#index"
   resources :films, only: [:index, :show]
