ea11bc0d18977482bcfe389c6d4ca5335e4dca9c
Peter Lemiszki <plemiszki@gmail.com>
2024-12-08 12:03:13 -0500

    reports summary - use axlsx

--- app/workers/create_reports_summary.rb
+++ app/workers/create_reports_summary.rb
@@ -1,49 +1,52 @@
 class CreateReportsSummary
   include Sidekiq::Worker
   include ActionView::Helpers::NumberHelper
+  include ExportSpreadsheetHelpers
   sidekiq_options retry: false
+
+  COLUMN_NAMES = [
+    'Title',
+    'Licensor',
+    'Owed',
+    'Reserves Liquated This Quarter',
+    'Remaining Reserves'
+  ]
 
   def perform(quarter, year, days_due, time_started)
     job = Job.find_by_job_id(time_started)
     job_folder = "#{Rails.root}/tmp/#{time_started}"
+    file_path = "#{job_folder}/Reports Summary.xlsx"
     FileUtils.mkdir_p("#{job_folder}")
 
-    require 'xlsx_writer'
-    doc = XlsxWriter.new
-    sheet = doc.add_sheet('Films')
-    column_names = [
-      'Title',
-      'Licensor',
-      'Owed',
-      'Reserves Liquated This Quarter',
-      'Remaining Reserves'
-    ]
-    sheet.add_row(column_names)
+    Axlsx::Package.new do |p|
+      p.workbook.add_worksheet(nane: "Films") do |sheet|
+        add_row(sheet, COLUMN_NAMES)
 
-    if days_due == 'all'
-      reports = RoyaltyReport.where(quarter: quarter, year: year).includes(:film)
-    else
-      reports = RoyaltyReport.where(quarter: quarter, year: year, films: { days_statement_due: days_due }).includes(film: [:licensor])
-    end
-    reports = reports.to_a.sort_by { |report| report.film.title }
-    reports.each do |report|
-      film = report.film
-      reserves_breakdown = report.get_reserves_breakdown
-      quarter_string = "Q#{report.quarter} #{report.year}"
-      sheet.add_row([
-        film.title,
-        film.licensor.try(:name) || '(None)',
-        report.joined_amount_due,
-        reserves_breakdown[quarter_string]['liquidated_this_quarter'],
-        reserves_breakdown[quarter_string]['total_reserves']
-      ])
-      job.update({ current_value: job.current_value + 1 })
-    end
+        if days_due == 'all'
+          reports = RoyaltyReport.where(quarter: quarter, year: year).includes(:film)
+        else
+          reports = RoyaltyReport.where(quarter: quarter, year: year, films: { days_statement_due: days_due }).includes(film: [:licensor])
+        end
 
-    job.update({ first_line: 'Saving Spreadsheet', second_line: false })
-    file_path = "#{job_folder}/Reports Summary.xlsx"
-    FileUtils.mv doc.path, file_path
-    doc.cleanup
+        reports = reports.to_a.sort_by { |report| report.film.title }
+        reports.each do |report|
+          film = report.film
+          reserves_breakdown = report.get_reserves_breakdown
+          quarter_string = "Q#{report.quarter} #{report.year}"
+          add_row(sheet, [
+            film.title,
+            film.licensor.try(:name) || '(None)',
+            report.joined_amount_due,
+            reserves_breakdown[quarter_string]['liquidated_this_quarter'],
+            reserves_breakdown[quarter_string]['total_reserves']
+          ])
+          job.update({ current_value: job.current_value + 1 })
+        end
+
+        job.update({ first_line: 'Saving Spreadsheet', second_line: false })
+        p.serialize(file_path)
+      end
+    end
 
     job.update({ first_line: 'Uploading to AWS' })
     s3 = Aws::S3::Resource.new(
