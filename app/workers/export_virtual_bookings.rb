a8edbfa88ce7a6eb66bb2b2454ac0ea337b924ce
Peter Lemiszki <plemiszki@gmail.com>
2024-12-10 22:00:00 -0500

    export virtual bookings - use axlsx

--- app/workers/export_virtual_bookings.rb
+++ app/workers/export_virtual_bookings.rb
@@ -1,67 +1,68 @@
 class ExportVirtualBookings
   include Sidekiq::Worker
+  include ExportSpreadsheetHelpers
   sidekiq_options retry: false
+
+  HEADERS = [
+    "Start Date",
+    "End Date",
+    "Film",
+    "Venue",
+    "City",
+    "State",
+    "Terms",
+    "Billing Name",
+    "Billing Address 1",
+    "Billing Address 2",
+    "Billing City",
+    "Billing State",
+    "Billing Zip",
+    "Billing Country",
+    "Email",
+    "Box Office Received",
+    "Box Office",
+    "Hosted By",
+    "URL"
+  ]
 
   def perform(booking_ids, time_started)
     job = Job.find_by_job_id(time_started)
     job_folder = "#{Rails.root}/tmp/#{time_started}"
     FileUtils.mkdir_p("#{job_folder}")
-
-    require 'xlsx_writer'
-    doc = XlsxWriter.new
-    sheet = doc.add_sheet("Invoices")
-    sheet.add_row([
-      "Start Date",
-      "End Date",
-      "Film",
-      "Venue",
-      "City",
-      "State",
-      "Terms",
-      "Billing Name",
-      "Billing Address 1",
-      "Billing Address 2",
-      "Billing City",
-      "Billing State",
-      "Billing Zip",
-      "Billing Country",
-      "Email",
-      "Box Office Received",
-      "Box Office",
-      "Hosted By",
-      "URL"
-    ])
-
-    bookings = VirtualBooking.where(id: booking_ids).order(:id).includes(:film, :venue)
-    bookings.each_with_index do |booking, booking_index|
-      venue = booking.venue
-      sheet.add_row([
-        booking.start_date,
-        booking.end_date,
-        { type: :String, value: booking.film.title },
-        venue.label,
-        booking.shipping_city,
-        booking.shipping_state,
-        booking.terms,
-        booking.billing_name,
-        booking.billing_address1,
-        booking.billing_address2,
-        booking.billing_city,
-        booking.billing_state,
-        booking.billing_zip,
-        booking.billing_country,
-        booking.email,
-        (booking.box_office_received ? 'Yes' : 'No'),
-        booking.box_office,
-        booking.host,
-        booking.url
-      ])
-      job.update({ current_value: booking_index + 1 })
-    end
-
     file_path = "#{job_folder}/virtual_bookings.xlsx"
-    FileUtils.mv doc.path, file_path
-    doc.cleanup
+
+    Axlsx::Package.new do |p|
+      p.workbook.add_worksheet(:name => "Invoices") do |sheet|
+        add_row(sheet, HEADERS)
+        bookings = VirtualBooking.where(id: booking_ids).order(:id).includes(:film, :venue)
+        bookings.each_with_index do |booking, booking_index|
+          venue = booking.venue
+          add_row(sheet, [
+            booking.start_date,
+            booking.end_date,
+            booking.film.title,
+            venue.label,
+            booking.shipping_city,
+            booking.shipping_state,
+            booking.terms,
+            booking.billing_name,
+            booking.billing_address1,
+            booking.billing_address2,
+            booking.billing_city,
+            booking.billing_state,
+            booking.billing_zip,
+            booking.billing_country,
+            booking.email,
+            (booking.box_office_received ? 'Yes' : 'No'),
+            booking.box_office,
+            booking.host,
+            booking.url
+          ])
+          job.update({ current_value: booking_index + 1 })
+        end
+        p.serialize(file_path)
+      end
+    end
 
     job.update({ first_line: "Uploading to AWS" })
     s3 = Aws::S3::Resource.new(
