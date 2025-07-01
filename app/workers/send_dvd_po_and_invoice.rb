782f6327e9a61a2185ac773deabb40bc058361be
Peter Lemiszki <plemiszki@gmail.com>
2025-07-01 08:46:47 -0400

    send dvd po and invoice - fix mailgun

--- app/workers/send_dvd_po_and_invoice.rb
+++ app/workers/send_dvd_po_and_invoice.rb
@@ -37,15 +37,14 @@ class SendDvdPoAndInvoice
       source_doc = 5533 + PurchaseOrder.find_by_sql("SELECT * FROM purchase_orders WHERE ship_date IS NOT NULL AND reporting_only != 't'").count
       purchase_order.create_shipping_files(pathname, source_doc)
       attachments = [File.open("#{pathname}/#{source_doc}_worderline.txt", "r"), File.open("#{pathname}/#{source_doc}_worder.txt", "r")]
-      message_params = {
-        from: current_user.email,
-        to: (ENV['TEST_MODE'] == 'true' ? ENV['TEST_MODE_EMAIL'] : 'fulfillment@theadsgroup.com'),
-        cc: current_user.email,
-        subject: "Film Movement Sales Order #{source_doc}",
-        text: "Please see attached shipping files.",
-        attachment: attachments
-      }
-      mg_client.send_message 'filmmovement.com', message_params unless Rails.env.test?
+      mb = Mailgun::MessageBuilder.new
+      mb.from(current_user.email)
+      mb.add_recipient(:to, (ENV['TEST_MODE'] == 'true' ? ENV['TEST_MODE_EMAIL'] : 'fulfillment@theadsgroup.com'))
+      mb.add_recipient(:cc, current_user.email)
+      mb.subject("Film Movement Sales Order #{source_doc}")
+      mb.body_text("Please see attached shipping files.")
+      attachments.each { |attachment| mb.add_attachment(attachment.path) }
+      mg_client.send_message 'filmmovement.com', mb unless Rails.env.test?
       purchase_order.decrement_stock!
       sent_shipping_files = true
     end
