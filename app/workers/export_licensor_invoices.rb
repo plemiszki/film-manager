30f41a438885d6f2ff268dcd1101d48d826eb79d
Peter Lemiszki <plemiszki@gmail.com>
2026-03-26 19:43:12 -0400

    export licensor invoices - new service

--- app/workers/export_licensor_invoices.rb
+++ app/workers/export_licensor_invoices.rb
@@ -1,8 +1,5 @@
 class ExportLicensorInvoices
   include Sidekiq::Worker
-  include ActionView::Helpers::NumberHelper
-  include ExportSpreadsheetHelpers
-  include AwsUpload
 
   sidekiq_options retry: false
 
@@ -82,16 +79,12 @@ class ExportLicensorInvoices
   }
 
   def perform(quarter, year, days_due, time_started)
-
     job = Job.find_by_job_id(time_started)
-    job_folder = "#{Rails.root}/tmp/#{time_started}"
-    file_path = "#{job_folder}/#{FILENAME}"
-    FileUtils.mkdir_p("#{job_folder}")
 
     if days_due == 'all'
       reports = RoyaltyReport.where(quarter: quarter, year: year, films: { export_reports: true }).includes(film: [:licensor])
     else
-      reports = RoyaltyReport.where(quarter: quarter, year: year, films: { export_reports: true,  days_statement_due: days_due }).includes(film: [:licensor])
+      reports = RoyaltyReport.where(quarter: quarter, year: year, films: { export_reports: true, days_statement_due: days_due }).includes(film: [:licensor])
     end
 
     amount_due_reports = reports.where('joined_amount_due > 0')
@@ -104,40 +97,36 @@ class ExportLicensorInvoices
       reports_by_licensor[report.film.licensor_id] << report
     end
 
-    Axlsx::Package.new do |p|
-      p.workbook.add_worksheet(:name => "Invoices") do |sheet|
-        add_row(sheet, COLUMN_HEADERS)
-        reports_by_licensor.each do |licensor_id, licensor_reports|
-          licensor_reports.each_with_index do |report, index|
-            film = report.film
-            days_due = film.days_statement_due
-            licensor = film.licensor
-            quarter_string = "Q#{report.quarter} #{report.year}"
+    rows = []
+    reports_by_licensor.each do |licensor_id, licensor_reports|
+      licensor_reports.each_with_index do |report, index|
+        film = report.film
+        licensor = film.licensor
+        quarter_string = "Q#{report.quarter} #{report.year}"
 
-            rowData = CONSTANT_DATA.merge({
-              "Vendor ID": licensor.sage_id,
-              "Invoice #": quarter_string,
-              "Date Due": (Date.today + 30.days).strftime("%-m/%-d/%Y"),
-              "Description": film.title,
-              "G/L Account": film.fm_subscription_only? ? "49350" : "49000",
-              "Job ID": film.get_sage_id,
-              "Invoice/CM Distribution": index + 1,
-              "Number of Distributions": licensor_reports.length,
-              "Date": Date.today.strftime("%-m/%-d/%Y"),
-              "Amount": { value: report.joined_amount_due.to_f, type: :float },
-            })
+        rowData = CONSTANT_DATA.merge({
+          "Vendor ID": licensor.sage_id,
+          "Invoice #": quarter_string,
+          "Date Due": (Date.today + 30.days).strftime("%-m/%-d/%Y"),
+          "Description": film.title,
+          "G/L Account": film.fm_subscription_only? ? "49350" : "49000",
+          "Job ID": film.get_sage_id,
+          "Invoice/CM Distribution": index + 1,
+          "Number of Distributions": licensor_reports.length,
+          "Date": Date.today.strftime("%-m/%-d/%Y"),
+          "Amount": { value: report.joined_amount_due.to_f, type: :float },
+        })
 
-            add_row(sheet, COLUMN_HEADERS.map { |column| rowData.fetch(column.to_sym, "") })
-            job.update({ current_value: job.current_value + 1 })
-          end
-        end
-        job.update({ first_line: 'Saving Spreadsheet', second_line: false })
-        p.serialize(file_path)
+        rows << COLUMN_HEADERS.map { |column| rowData.fetch(column.to_sym, "") }
       end
     end
 
-    job.update({ first_line: 'Uploading to AWS' })
-    public_url = upload_to_aws(file_path: file_path, key: "#{time_started}/#{FILENAME}")
+    public_url = ExportAndUploadSpreadsheet.new(
+      headers:  COLUMN_HEADERS,
+      rows:     rows,
+      job:      job,
+      filename: FILENAME
+    ).call
 
     job.update!({ status: :success, metadata: { url: public_url } })
   end
