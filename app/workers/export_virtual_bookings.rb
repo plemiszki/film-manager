ed0f83cde5a1c27ec5a9f2e3262cb49841bca6c0
Peter Lemiszki <plemiszki@gmail.com>
2026-03-26 19:52:00 -0400

    export virtual bookings - new service

--- app/workers/export_virtual_bookings.rb
+++ app/workers/export_virtual_bookings.rb
@@ -1,7 +1,5 @@
 class ExportVirtualBookings
   include Sidekiq::Worker
-  include ExportSpreadsheetHelpers
-  include AwsUpload
 
   sidekiq_options retry: false
 
@@ -29,44 +27,39 @@ class ExportVirtualBookings
 
   def perform(booking_ids, time_started)
     job = Job.find_by_job_id(time_started)
-    job_folder = "#{Rails.root}/tmp/#{time_started}"
-    FileUtils.mkdir_p("#{job_folder}")
-    file_path = "#{job_folder}/virtual_bookings.xlsx"
 
-    Axlsx::Package.new do |p|
-      p.workbook.add_worksheet(:name => "Invoices") do |sheet|
-        add_row(sheet, HEADERS)
-        bookings = VirtualBooking.where(id: booking_ids).order(:id).includes(:film, :venue)
-        bookings.each_with_index do |booking, booking_index|
-          venue = booking.venue
-          add_row(sheet, [
-            booking.start_date,
-            booking.end_date,
-            booking.film.title,
-            venue.label,
-            booking.shipping_city,
-            booking.shipping_state,
-            booking.terms,
-            booking.billing_name,
-            booking.billing_address1,
-            booking.billing_address2,
-            booking.billing_city,
-            booking.billing_state,
-            booking.billing_zip,
-            booking.billing_country,
-            booking.email,
-            (booking.box_office_received ? 'Yes' : 'No'),
-            booking.box_office,
-            booking.host,
-            booking.url
-          ])
-          job.update({ current_value: booking_index + 1 })
-        end
-        p.serialize(file_path)
-      end
+    bookings = VirtualBooking.where(id: booking_ids).order(:id).includes(:film, :venue)
+    rows = bookings.map do |booking|
+      venue = booking.venue
+      [
+        booking.start_date,
+        booking.end_date,
+        booking.film.title,
+        venue.label,
+        booking.shipping_city,
+        booking.shipping_state,
+        booking.terms,
+        booking.billing_name,
+        booking.billing_address1,
+        booking.billing_address2,
+        booking.billing_city,
+        booking.billing_state,
+        booking.billing_zip,
+        booking.billing_country,
+        booking.email,
+        (booking.box_office_received ? 'Yes' : 'No'),
+        booking.box_office,
+        booking.host,
+        booking.url
+      ]
     end
 
-    public_url = upload_to_aws(file_path: file_path, key: "#{time_started}/virtual_bookings.xlsx")
+    public_url = ExportAndUploadSpreadsheet.new(
+      headers:  HEADERS,
+      rows:     rows,
+      job:      job,
+      filename: 'virtual_bookings.xlsx'
+    ).call
 
     job.update!({ status: 'success', first_line: '', metadata: { url: public_url }, errors_text: '' })
   end
