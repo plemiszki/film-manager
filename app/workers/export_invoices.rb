f8b9546dfe4333d956877e54210bad9352304fcc
Peter Lemiszki <plemiszki@gmail.com>
2026-03-26 19:36:43 -0400

    export invoices - use service

--- app/workers/export_invoices.rb
+++ app/workers/export_invoices.rb
@@ -2,100 +2,97 @@ include InvoiceImportHelper
 
 class ExportInvoices
   include Sidekiq::Worker
-  include ExportSpreadsheetHelpers
-  include AwsUpload
 
   sidekiq_options retry: false
 
   def perform(invoice_ids, time_started)
     errors = []
     job = Job.find_by_job_id(time_started)
-    job_folder = "#{Rails.root}/tmp/#{time_started}"
-    FileUtils.mkdir_p("#{job_folder}")
-    file_path = "#{job_folder}/invoices.xlsx"
 
-    Axlsx::Package.new do |p|
-      p.workbook.add_worksheet(:name => "Invoices") do |sheet|
-        add_row(sheet, COLUMN_HEADERS)
-        invoices = Invoice.where(id: invoice_ids).order(:id)
-        invoices.each_with_index do |invoice, invoice_index|
-          items = invoice.invoice_rows
-          items.each_with_index do |item, index|
-            rowData = CONSTANT_DATA.merge({
-              "Invoice/CM #": invoice.number,
-              "Invoice/CM Distribution": index,
-              "Number of Distributions": items.length,
-              "Date": invoice.sent_date.strftime("%-m/%-d/%Y"),
-              "Ship to Name": invoice.shipping_name,
-              "Ship to Address-Line One": invoice.shipping_address1,
-              "Ship to Address-Line Two": invoice.shipping_address2,
-              "Ship to City": invoice.shipping_city,
-              "Ship to State": invoice.shipping_state,
-              "Ship to Zipcode": invoice.shipping_zip,
-              "Ship to Country": invoice.shipping_country,
-              "Customer PO": invoice.po_number,
-              "Quantity": { value: item.item_qty, type: :integer },
-              "Unit Price": { value: (item.unit_price * -1), type: :float },
-              "Amount": { value: (item.total_price * -1), type: :float },
-            })
+    invoices = Invoice.where(id: invoice_ids).order(:id)
+    rows = []
+    invoices.each do |invoice|
+      items = invoice.invoice_rows
+      items.each_with_index do |item, index|
+        rowData = CONSTANT_DATA.merge({
+          "Invoice/CM #": invoice.number,
+          "Invoice/CM Distribution": index,
+          "Number of Distributions": items.length,
+          "Date": invoice.sent_date.strftime("%-m/%-d/%Y"),
+          "Ship to Name": invoice.shipping_name,
+          "Ship to Address-Line One": invoice.shipping_address1,
+          "Ship to Address-Line Two": invoice.shipping_address2,
+          "Ship to City": invoice.shipping_city,
+          "Ship to State": invoice.shipping_state,
+          "Ship to Zipcode": invoice.shipping_zip,
+          "Ship to Country": invoice.shipping_country,
+          "Customer PO": invoice.po_number,
+          "Quantity": { value: item.item_qty, type: :integer },
+          "Unit Price": { value: (item.unit_price * -1), type: :float },
+          "Amount": { value: (item.total_price * -1), type: :float },
+        })
 
-            case invoice.invoice_type
-            when 'dvd'
-              rowData = rowData.merge({
-                "Customer ID": invoice.customer.sage_id,
-                "Date Due": (invoice.sent_date + invoice.payment_terms).strftime("%-m/%-d/%Y"),
-                "Displayed Terms": "Net #{invoice.payment_terms}",
-                "Description": item.item_label,
-                "G/L Account": "30200",
-                "Job ID": (item.item_type == 'dvd' ? Film.find(item.item_id).get_sage_id : Giftbox.find(item.item_id).sage_id),
-              })
-            when 'booking'
-              booking = invoice.booking
-              unless booking
-                errors << "Missing Booking for Invoice #{invoice.number}"
-                next
-              end
-              booking_venue = booking.venue
-              booking_film = booking.film
-              booking_gl_code = get_gl_code(booking)
-              errors << "No Sage ID for #{booking_venue.label}" if booking_venue.sage_id.empty?
-              is_shipping_fee = item.item_label == 'Shipping Fee'
-              rowData = rowData.merge({
-                "Customer ID": booking_venue.sage_id,
-                "Date Due": (invoice.sent_date + 30).strftime("%-m/%-d/%Y"),
-                "Displayed Terms": "Net 30",
-                "Description": "#{booking_film.title} #{booking.start_date.strftime("%-m/%-d/%Y")} - #{booking.end_date.strftime("%-m/%-d/%Y")}",
-                "G/L Account": is_shipping_fee ? '40069' : booking_gl_code,
-                "Job ID": is_shipping_fee ? '' : booking_film.get_sage_id,
-              })
-            when 'institution'
-              institution = invoice.institution
-              errors << "No Sage ID for #{institution.label}" if institution.sage_id.empty?
-              is_shipping_fee = item.item_label == 'Shipping Fee'
-              rowData = rowData.merge({
-                "Customer ID": institution.sage_id,
-                "Date Due": (invoice.sent_date + 30).strftime("%-m/%-d/%Y"),
-                "Displayed Terms": "Net 30",
-                "Description": item.item_label,
-                "G/L Account": is_shipping_fee ? '40069' : "30440",
-                "Job ID": is_shipping_fee ? '' : Film.find(item.item_id).get_sage_id,
-              })
-            end
-            add_row(sheet, COLUMN_HEADERS.map { |column| rowData.fetch(column.to_sym, "") })
+        case invoice.invoice_type
+        when 'dvd'
+          rowData = rowData.merge({
+            "Customer ID": invoice.customer.sage_id,
+            "Date Due": (invoice.sent_date + invoice.payment_terms).strftime("%-m/%-d/%Y"),
+            "Displayed Terms": "Net #{invoice.payment_terms}",
+            "Description": item.item_label,
+            "G/L Account": "30200",
+            "Job ID": (item.item_type == 'dvd' ? Film.find(item.item_id).get_sage_id : Giftbox.find(item.item_id).sage_id),
+          })
+        when 'booking'
+          booking = invoice.booking
+          unless booking
+            errors << "Missing Booking for Invoice #{invoice.number}"
+            next
           end
-          job.update({ current_value: invoice_index + 1 })
+          booking_venue = booking.venue
+          booking_film = booking.film
+          booking_gl_code = get_gl_code(booking)
+          errors << "No Sage ID for #{booking_venue.label}" if booking_venue.sage_id.empty?
+          is_shipping_fee = item.item_label == 'Shipping Fee'
+          rowData = rowData.merge({
+            "Customer ID": booking_venue.sage_id,
+            "Date Due": (invoice.sent_date + 30).strftime("%-m/%-d/%Y"),
+            "Displayed Terms": "Net 30",
+            "Description": "#{booking_film.title} #{booking.start_date.strftime("%-m/%-d/%Y")} - #{booking.end_date.strftime("%-m/%-d/%Y")}",
+            "G/L Account": is_shipping_fee ? '40069' : booking_gl_code,
+            "Job ID": is_shipping_fee ? '' : booking_film.get_sage_id,
+          })
+        when 'institution'
+          institution = invoice.institution
+          errors << "No Sage ID for #{institution.label}" if institution.sage_id.empty?
+          is_shipping_fee = item.item_label == 'Shipping Fee'
+          rowData = rowData.merge({
+            "Customer ID": institution.sage_id,
+            "Date Due": (invoice.sent_date + 30).strftime("%-m/%-d/%Y"),
+            "Displayed Terms": "Net 30",
+            "Description": item.item_label,
+            "G/L Account": is_shipping_fee ? '40069' : "30440",
+            "Job ID": is_shipping_fee ? '' : Film.find(item.item_id).get_sage_id,
+          })
         end
-        p.serialize(file_path)
+
+        rows << COLUMN_HEADERS.map { |column| rowData.fetch(column.to_sym, "") }
       end
     end
 
     if errors.present?
       job.update!({ status: 'failed', first_line: 'Errors Found', errors_text: errors.uniq.join("\n") })
-    else
-      job.update({ first_line: "Uploading to AWS" })
-      public_url = upload_to_aws(file_path: file_path, key: "#{time_started}/invoices.xlsx")
-      job.update!({ status: 'success', first_line: '', metadata: { url: public_url }, errors_text: '' })
+      return
     end
+
+    public_url = ExportAndUploadSpreadsheet.new(
+      headers:              COLUMN_HEADERS,
+      rows:                 rows,
+      job:                  job,
+      filename:             'invoices.xlsx',
+      increment_job_column: 'Invoice/CM #'
+    ).call
+
+    job.update!({ status: 'success', first_line: '', metadata: { url: public_url }, errors_text: '' })
   end
 
   def get_gl_code(booking)
