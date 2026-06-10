db5fdf16c2164848917243c864e976106b9f0c0b
Peter Lemiszki <plemiszki@gmail.com>
2026-06-10 14:59:01 -0400

    report details - fix

--- app/models/royalty_report.rb
+++ app/models/royalty_report.rb
@@ -330,7 +330,7 @@ class RoyaltyReport < ActiveRecord::Base
       report = RoyaltyReport.find_by(year: year, quarter: quarter, film_id: film.id)
       next unless report
       report_streams = report.royalty_revenue_streams
-      result.update({
+      result.assign_attributes({
         current_total: result.current_total + report.current_total,
         current_total_revenue: result.current_total_revenue + report.current_total_revenue,
         current_total_expenses: result.current_total_expenses + report.current_total_expenses,
@@ -351,7 +351,7 @@ class RoyaltyReport < ActiveRecord::Base
       })
       report_streams.each_with_index do |report_stream, index|
         stream = streams[index]
-        stream.update({
+        stream.assign_attributes({
           current_revenue: stream.current_revenue + report_stream.current_revenue,
           current_gr: stream.current_gr + report_stream.current_gr,
           current_expense: stream.current_expense + report_stream.current_expense,
